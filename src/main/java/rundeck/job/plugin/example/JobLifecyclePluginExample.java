4dccbe2e1273373f8005d25ad86a980a6647a8b9
Alberto Hormazabal <alberto@exatech.cl>
2019-11-04 14:22:57 -0300

    minor improvements

--- src/main/java/rundeck/job/plugin/example/JobLifecyclePluginExample.java
+++ src/main/java/rundeck/job/plugin/example/JobLifecyclePluginExample.java
@@ -24,7 +24,8 @@ import com.dtolabs.rundeck.plugins.project.JobLifecyclePlugin;
 public class JobLifecyclePluginExample implements JobLifecyclePlugin{
 
 	private static final String EXAMPLE_OPTION_NAME = "exampleOptionName";
-	
+	private static final String EXAMPLE_OPTION_VALUE = "exampleOptionValue";
+
 	class JobEventStatusImpl implements JobLifecycleStatus {
 
 		private boolean successful = true;
@@ -32,7 +33,7 @@ public class JobLifecyclePluginExample implements JobLifecyclePlugin{
 		private Map optionValues = new HashMap<String, String>();
 		private SortedSet<JobOption> options = new TreeSet<JobOption>();
 		private String errorDescription;
-		
+
 		@Override
 		public SortedSet<JobOption> getOptions() {
 			return options;
@@ -67,9 +68,9 @@ public class JobLifecyclePluginExample implements JobLifecyclePlugin{
 		public String getErrorMessage() {
 			return this.errorDescription;
 		}
-		
+
 	}
-	
+
 	//It triggers before the job execution
 	//It can prevent the job for being executed and it can also change the options values
 	@Override
@@ -78,7 +79,7 @@ public class JobLifecyclePluginExample implements JobLifecyclePlugin{
 		for(INodeEntry node :event.getNodes().getNodes()) {
 			System.out.println(node.getTags());
 			Map<String, String> attributes = node.getAttributes();
-			for(String key : attributes.keySet()) { 
+			for(String key : attributes.keySet()) {
 				System.out.println(attributes.get(key));
 			}
 		}
@@ -90,33 +91,34 @@ public class JobLifecyclePluginExample implements JobLifecyclePlugin{
 				//It checks if the job have the option "exampleOptionName", if exist, it checks for it to have a value
 				if(option.getName().equals(EXAMPLE_OPTION_NAME)) {
 					result.getOptionsValues().put(option.getName(), event.getOptionsValues().get(EXAMPLE_OPTION_NAME));
-					if(!event.getOptionsValues().containsKey(EXAMPLE_OPTION_NAME) || ((String)event.getOptionsValues().get(EXAMPLE_OPTION_NAME)).trim().isEmpty()) {
+					if(!event.getOptionsValues().containsKey(EXAMPLE_OPTION_NAME)
+							|| !((String)event.getOptionsValues().get(EXAMPLE_OPTION_NAME)).trim().equalsIgnoreCase(EXAMPLE_OPTION_VALUE)) {
 						//It indicates that the job should not start, since the "exampleOptionName" does not have a value
-						result.setErrorMessage(EXAMPLE_OPTION_NAME + " must have a value");
+						result.setErrorMessage(EXAMPLE_OPTION_NAME + " must have a valid value");
 						result.setSuccessful(false);
 					}
 				}else {
 					//This will change the value of every other option
-					result.getOptionsValues().put(option.getName(), "new value for other options");
+//					result.getOptionsValues().put(option.getName(), "new value for other options");
 				}
 			}
 		}
 		return result;
 	}
-	
+
 	//It runs before a job is persisted
 	//It can add or remove job options
 	//In this example, we will always add the "exampleOptionName" if it does not exist in the job
 	@Override
 	public JobLifecycleStatus beforeSaveJob(JobPersistEvent event) throws JobLifecyclePluginException {
-		
+
 		//It returns the node filter for the job
-		//event.getNodeFilter(); 
+		//event.getNodeFilter();
 		//It returns the nodes obtained by the node filter
 		//event.getNodes();
 		JobEventStatusImpl result = new JobEventStatusImpl();
 		boolean addExampleOption = true;
-		
+
 		//Iterates over job options and adds every option to the result
 		//if an option is not added to the result it will be removed from the job
 		if(event.getOptions() != null) {
@@ -129,12 +131,13 @@ public class JobLifecyclePluginExample implements JobLifecyclePlugin{
 				}
 			}
 		}
-		
+
 		//Creates a new job option and adds it to the result
 		if(addExampleOption) {
 			JobOptionImplBuilder builder = JobOptionImpl.builder();
 			//Option name
 			builder.name(EXAMPLE_OPTION_NAME);
+			builder.defaultValue(EXAMPLE_OPTION_VALUE);
 			//It indicates if the option is mandatory
 			builder.required(true);
 			//Security level
@@ -149,7 +152,7 @@ public class JobLifecyclePluginExample implements JobLifecyclePlugin{
 			//it indicates that the new values should be used
 			result.setUseNewValues(true);
 		}
-		
+
 		return result;
-	}	
+	}
 }
